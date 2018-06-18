6c680bf79b6233c25a62c6a27ef475467043874b
Kosuke Hata <hata.kosuke@gmail.com>
2018-06-18 14:13:06 -0700

    adding oh my zsh and antigen by default

---
+++ .bashrc
@@ -0,0 +1,3 @@
+export GOROOT=/usr/lib/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
--- install.sh
+++ install.sh
@@ -6,35 +6,47 @@
 
 ########## Variables
  
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim"        # list of files/folders to symlink in homedir
+DIR=~/dotfiles                    # dotfiles directory
+OLD_DIR=~/dotfiles_old             # old dotfiles backup directory
+FILES=".zshrc .bashrc .vimrc .vim"        # list of files/folders to symlink in homedir
  
 ##########
  
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $OLD_DIR for backup of any existing dotfiles in ~"
+mkdir -p $OLD_DIR
 echo "...done"
  
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $DIR directory"
+cd $DIR
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+for FILE in $FILES; do
+    echo "Moving any existing dotfiles from ~ to $OLD_DIR"
+    mv ~/$FILE ~/dotfiles_old/
+    echo "Creating symlink to $FILE in home directory."
+    ln -s $dir/$FILE ~/$FILE
 done
  
 source ~/.bashrc
 source ~/.vimrc
+source ~/.zshrc
 
 # download vundle
 echo "Downloading Vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle.vim
 echo "...done"
  
+# install oh my zsh
+echo "Downloading Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "...done"
+
+# install antigen
+echo "Downloading Antigen..."
+mkdir -p ~/antigen
+curl -L git.io/antigen > ~/antigen/antigen.zsh
+echo "...done"
+
