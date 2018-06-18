602664490a5eb2720b1c36f454bbe014459d028b
Kosuke Hata <hata.kosuke@gmail.com>
2018-06-18 14:28:56 -0700

    updating install.sh

--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="~/.oh-my-zsh"
+export ZSH="/home/kosuke/.oh-my-zsh"
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
@@ -106,6 +106,11 @@ antigen theme refined
 # antigen custom  bundles
 antigen bundle zsh-users/zsh-syntax-highlighting
 
-# tell antigen that you are done, and ready to apply
+# tel antigen that you are done, and ready to apply
 antigen apply
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/kosuke/google-cloud-sdk/path.zsh.inc' ]; then source '/home/kosuke/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/kosuke/google-cloud-sdk/completion.zsh.inc' ]; then source '/home/kosuke/google-cloud-sdk/completion.zsh.inc'; fi
--- install.sh
+++ install.sh
@@ -4,49 +4,56 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
+### Variables ###
  
 DIR=~/dotfiles                    # dotfiles directory
 OLD_DIR=~/dotfiles_old             # old dotfiles backup directory
 FILES=".zshrc .bashrc .vimrc .vim"        # list of files/folders to symlink in homedir
  
-##########
- 
+### Download packages ###
+
+# install vundle
+echo ""
+echo "=== Downloading Vundle === "
+git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle.vim
+echo "...done"
+echo ""
+
+# install oh my zsh
+echo ""
+echo "=== Downloading Oh My Zsh ==="
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "...done"
+echo ""
+
+# install antigen
+echo ""
+echo "=== Downloading Antigen ==="
+mkdir -p ~/antigen
+curl -L git.io/antigen > ~/antigen/antigen.zsh
+echo "...done"
+echo ""
+
 # create dotfiles_old in homedir
+echo ""
 echo "Creating $OLD_DIR for backup of any existing dotfiles in ~"
 mkdir -p $OLD_DIR
 echo "...done"
- 
+echo ""
+
 # change to the dotfiles directory
+echo ""
 echo "Changing to the $DIR directory"
 cd $DIR
 echo "...done"
+echo ""
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo ""
 for FILE in $FILES; do
     echo "Moving any existing dotfiles from ~ to $OLD_DIR"
     mv ~/$FILE ~/dotfiles_old/
     echo "Creating symlink to $FILE in home directory."
-    ln -s $dir/$FILE ~/$FILE
+    ln -s $DIR/$FILE ~/$FILE
 done
- 
-source ~/.bashrc
-source ~/.vimrc
-source ~/.zshrc
-
-# download vundle
-echo "Downloading Vundle..."
-git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle.vim
-echo "...done"
- 
-# install oh my zsh
-echo "Downloading Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "...done"
-
-# install antigen
-echo "Downloading Antigen..."
-mkdir -p ~/antigen
-curl -L git.io/antigen > ~/antigen/antigen.zsh
-echo "...done"
-
+echo ""
